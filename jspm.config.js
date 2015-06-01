71506af099d150792dbc9eff2b1250e94a49ebce
Douglas Duteil <douglasduteil@gmail.com>
2015-06-01 23:22:13 +0200

    chore(jspm): update meta info in package

--- jspm.config.js
+++ jspm.config.js
@@ -17,20 +17,19 @@ System.config({
 System.config({
   "map": {
     "angular": "github:angular/bower-angular@1.3.15",
-    "babel": "npm:babel-core@5.0.12",
-    "babel-runtime": "npm:babel-runtime@5.0.12",
+    "babel": "npm:babel-core@5.4.7",
+    "babel-runtime": "npm:babel-runtime@5.4.7",
     "camelcase": "npm:camelcase@1.0.2",
-    "core-js": "npm:core-js@0.8.1",
+    "core-js": "npm:core-js@0.9.13",
     "lodash": "npm:lodash@3.6.0",
     "lower-case-first": "npm:lower-case-first@1.0.0",
     "github:jspm/nodelibs-process@0.1.1": {
       "process": "npm:process@0.10.1"
     },
-    "npm:babel-runtime@4.7.16": {
-      "process": "github:jspm/nodelibs-process@0.1.1"
-    },
-    "npm:core-js@0.8.1": {
-      "process": "github:jspm/nodelibs-process@0.1.1"
+    "npm:core-js@0.9.13": {
+      "fs": "github:jspm/nodelibs-fs@0.1.2",
+      "process": "github:jspm/nodelibs-process@0.1.1",
+      "systemjs-json": "github:systemjs/plugin-json@0.1.0"
     },
     "npm:lodash@3.6.0": {
       "process": "github:jspm/nodelibs-process@0.1.1"
